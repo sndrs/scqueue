005600b5225f716d29d222328d03f7c8f27bda79
sndrs <sndrs@me.com>
2013-07-28 21:38:10 +0100

    not needed now

--- app/scripts/bookmarklet.js
+++
@@ -1,6 +0,0 @@
-(function(){
-  var iframe = document.createElement('iframe');
-  iframe.src = '//localhost:9000/bookmarklet.html';
-  iframe.style.display = 'none';
-  document.body.appendChild(iframe);
-})();
